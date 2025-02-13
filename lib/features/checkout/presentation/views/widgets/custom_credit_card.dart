21571152d57211a0098df88619837337a3e1e12b
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2025-02-14 01:10:34 +0200

    Add flutter stripe and make the configration

--- lib/features/checkout/presentation/views/widgets/custom_credit_card.dart
+++ lib/features/checkout/presentation/views/widgets/custom_credit_card.dart
@@ -1,7 +1,7 @@
 import 'package:checkout_app/core/utils/app_images.dart';
 import 'package:checkout_app/core/utils/app_style.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_credit_card/flutter_credit_card.dart';
+// import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:flutter_svg/svg.dart';
 
 class CustomCreditCard extends StatefulWidget {
@@ -25,49 +25,49 @@ class _CustomCreditCardState extends State<CustomCreditCard> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CreditCardWidget(
-          cardNumber: cardNumber,
-          expiryDate: expiryDate,
-          cardHolderName: cardHolderName,
-          cvvCode: cvvCode,
-          showBackView: showBackView,
-          isHolderNameVisible: true,
-          onCreditCardWidgetChange: (CreditCardBrand creditCardBrand) {},
-          backgroundImage: Assets.imagesBackgoundCard,
-          bankName: 'Credit',
-          // glassmorphismConfig: Glassmorphism.defaultConfig(),
-          isChipVisible: false,
-          textStyle: AppStyle.styleSemiBold14,
-          cardType: CardType.mastercard,
-          customCardTypeIcons: [
-            CustomCardTypeIcon(
-              cardImage: SvgPicture.asset(Assets.imagesMastercardLogo),
-              cardType: CardType.mastercard,
-            )
-          ],
-          // enableFloatingCard: true,
-          cardBgColor: Colors.white.withOpacity(0.0),
-          isSwipeGestureEnabled: false,
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        CreditCardForm(
-          cardNumber: cardNumber,
-          expiryDate: expiryDate,
-          cardHolderName: cardHolderName,
-          cvvCode: cvvCode,
-          autovalidateMode: widget.autovalidateMode,
-          onCreditCardModelChange: (CreditCardModel creditCardModel) {
-            cardNumber = creditCardModel.cardNumber;
-            expiryDate = creditCardModel.expiryDate;
-            cardHolderName = creditCardModel.cardHolderName;
-            cvvCode = creditCardModel.cvvCode;
-            showBackView = creditCardModel.isCvvFocused;
-            setState(() {});
-          },
-          formKey: widget.formKey,
-        ),
+        // CreditCardWidget(
+        //   cardNumber: cardNumber,
+        //   expiryDate: expiryDate,
+        //   cardHolderName: cardHolderName,
+        //   cvvCode: cvvCode,
+        //   showBackView: showBackView,
+        //   isHolderNameVisible: true,
+        //   onCreditCardWidgetChange: (CreditCardBrand creditCardBrand) {},
+        //   backgroundImage: Assets.imagesBackgoundCard,
+        //   bankName: 'Credit',
+        //   // glassmorphismConfig: Glassmorphism.defaultConfig(),
+        //   isChipVisible: false,
+        //   textStyle: AppStyle.styleSemiBold14,
+        //   cardType: CardType.mastercard,
+        //   customCardTypeIcons: [
+        //     CustomCardTypeIcon(
+        //       cardImage: SvgPicture.asset(Assets.imagesMastercardLogo),
+        //       cardType: CardType.mastercard,
+        //     )
+        // ],
+        // // enableFloatingCard: true,
+        // cardBgColor: Colors.white.withOpacity(0.0),
+        // isSwipeGestureEnabled: false,
+        // ),
+        // const SizedBox(
+        //   height: 12,
+        // ),
+        // CreditCardForm(
+        //   cardNumber: cardNumber,
+        //   expiryDate: expiryDate,
+        //   cardHolderName: cardHolderName,
+        //   cvvCode: cvvCode,
+        //   autovalidateMode: widget.autovalidateMode,
+        //   onCreditCardModelChange: (CreditCardModel creditCardModel) {
+        //     cardNumber = creditCardModel.cardNumber;
+        //     expiryDate = creditCardModel.expiryDate;
+        //     cardHolderName = creditCardModel.cardHolderName;
+        //     cvvCode = creditCardModel.cvvCode;
+        //     showBackView = creditCardModel.isCvvFocused;
+        //     setState(() {});
+        //   },
+        //   formKey: widget.formKey,
+        // ),
       ],
     );
   }
