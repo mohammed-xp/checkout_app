5266db33572d9d459a1c303e62424b1f816cf79b
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2025-02-13 02:55:50 +0200

    Add payment methods bottom sheet to MyCartView for improved payment selection

--- lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:checkout_app/core/utils/app_images.dart';
 import 'package:checkout_app/features/checkout/presentation/views/payment_details_view.dart';
 import 'package:checkout_app/features/checkout/presentation/views/widgets/custom_button.dart';
+import 'package:checkout_app/features/checkout/presentation/views/widgets/payment_methods.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_divider.dart';
@@ -43,13 +44,50 @@ class MyCartViewBody extends StatelessWidget {
           CustomButton(
             title: 'Complete Payment',
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const PaymentDetailsView();
-              }));
+              // Navigator.push(context, MaterialPageRoute(builder: (context) {
+              //   return const PaymentDetailsView();
+              // }));
+
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => const PaymentMethodsBottomSheet(),
+              );
             },
           ),
           const SizedBox(
             height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PaymentMethodsBottomSheet extends StatelessWidget {
+  const PaymentMethodsBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(
+            height: 16,
+          ),
+          const PaymentMethods(),
+          const SizedBox(height: 32),
+          CustomButton(
+            title: 'Continue',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const PaymentDetailsView(),
+                ),
+              );
+            },
           ),
         ],
       ),
